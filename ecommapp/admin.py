9edf1e5ffcad9503285ad75f64bdd394689eb02f
Sudeep <sudeepsavai16@gmail.com>
2023-07-26 22:05:56 +0530

    payment int

--- ecommapp/admin.py
+++ ecommapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Product,Customer,Cart
+from . models import Product,Customer,Cart,Payment,OrderPlaced
 # Register your models here.
 @admin.register(Product)
 class ProductAdminModel(admin.ModelAdmin):
@@ -12,3 +12,11 @@ class CustomerAdminModel(admin.ModelAdmin):
 @admin.register(Cart)
 class CartAdminModel(admin.ModelAdmin):
     list_display = ['id','user','product','quantity']
+
+@admin.register(Payment)
+class PaymentAdminModel(admin.ModelAdmin):
+    list_display = ['id','user','amount','razorpay_order_id','razorpay_payment_status','razorpay_payment_id','paid']
+
+@admin.register(OrderPlaced)
+class OrderPlacedAdminModel(admin.ModelAdmin):  
+    list_display = ['id','user','customer','product','quantity','order_date','status','payment']
--- ecommapp/models.py
+++ ecommapp/models.py
@@ -55,7 +55,15 @@ STATE_CHOICES = [
     ('Puducherry','Puducherry'),
   
 ]
-    
+
+STATUS_CHOICES=[
+    ('Packed','Packed'),
+    ('Pending','Pending'),
+    ('Delivered','Delivered'),
+    ('On The Way','On The Way'),
+    ('Accepted','Accepted'),
+    ('Cancel','Cancel'),
+]
 
 
 class Product(models.Model):
@@ -88,5 +96,23 @@ class Cart(models.Model):
     @property
     def total_cost(self):
         return self.quantity * self.product.price
+
+class Payment(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    amount=models.FloatField()
+    razorpay_order_id=models.CharField(max_length=100,blank=True,null=True)
+    razorpay_payment_status=models.CharField(max_length=100,blank=True,null=True)
+    razorpay_payment_id=models.CharField(max_length=100,blank=True,null=True)
+    paid=models.BooleanField(default=False)
+
+class OrderPlaced(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    order_date=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(max_length=50,choices=STATUS_CHOICES,default='Pending')
+    payment=models.ForeignKey(Payment,on_delete=models.CASCADE,default='')
+    
 
     
